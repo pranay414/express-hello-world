a13e03ccd4e56e37f3270cd6c1a1b3af56424002
Pranay Ankit <pranayankit414@gmail.com>
2018-11-03 01:25:53 +0530

    added useful comments

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // Import express module
+const app = express(); // Create app instance
+const path = require('path'); // Work with directories/files
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080; // Specify port, process.env.PORT is used for dynamic PORT allocation
 
+app.use(express.static(path.join(__dirname, 'client'))); // Apply middleware to serve static assets
+
+// Basic GET request to '/' endpoint
 app.get('/', (req, res) => {
-    res.send('Hello this is my first express app!');
+    res.send('index.html');
 });
 
-app.get('/pranay', (req, res) => {
-    res.send('This is Pranay endpoint!');
-});
-
+// Make your App listen on PORT, default: 8080 or else process.env.PORT
 app.listen(PORT, () => {
     console.log(`Express server running ${PORT}`);
 });
 
+// Alternative method of making your App listen on PORT, default: 8080 or else process.env.PORT
 /* app.listen(PORT, function() {
     console.log(`Express server running ${PORT}`);
 }); */
